b349a20fb3e898109322c67e698f9d33a677c12a
Marcus Del Vecchio <marcusdel2112@rogers.com>
2023-11-12 19:32:41 -0500

    add Clsoe String problem

--- strings.py
+++ strings.py
@@ -1,4 +1,5 @@
 # given two strings, returns true if they are one edit (insert, remove, replace) away
+# untested because 
 def areOneAway(str1, str2):
     remainingEdits = 1
 
@@ -38,6 +39,10 @@ def areOneAway(str1, str2):
 
 # test are one away function
 def areOneAway_test():
+    test0_1 = "the same string"
+    test0_2 = "the same string"
+    print(areOneAway(test0_1, test0_2)) # false
+
     test1_1 = "abcdef"
     test1_2 = "abcdxf"
     # print(areOneAway(test1_1, test1_2)) # true
@@ -53,10 +58,33 @@ def areOneAway_test():
     test4_1 = "abcdde"
     test4_2 = "abcdef"
     # print(areOneAway(test4_1, test4_2)) # false
-
-
-
     
+# Leetcode Determine is two strings are close question
+# https://leetcode.com/problems/determine-if-two-strings-are-close/
+def closeStrings(self, word1: str, word2: str) -> bool:
+    if len(word1) != len(word2):
+        return False
+
+    index = 0
+    remainingSingleSwaps = 1
+    swaps = {} # dictionary in the format 'word1Char':'word2Char' when two chars are incompatible
+    while index < len(word1):
+        if word1[index] != word2[index]:
+            keys = list(swap.keys())
+            if len(keys):
+                if keys[0] == word1[index] and swap[keys[0]] == word2[index]:
+                    continue
+                elif keys[0] == word2[index] and swap[keys[0]] == word1[index] and remainingSingleSwaps:
+                    remainingSingleSwaps -= 1
+                    continue
+                else:
+                    return False
+            # no keys added to swaps yet
+            else:
+                swaps[word1[index]] = word2[index]
+        else:
+            index += 1
+
 
 
 areOneAway_test()
