1796f865b214fd88aa7052aa1983bcca88d61145
Marcus Del Vecchio <marcusdel2112@rogers.com>
2023-11-13 21:06:11 -0500

    minor adjustments to reverseKGroup

--- linked-lists.py
+++ linked-lists.py
@@ -614,7 +614,7 @@ def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
 # https://leetcode.com/problems/reverse-nodes-in-k-group/description/
 # given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
 # k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
-def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+def reverseKGroup(self, head, k):
     
     # function to reverse a linked list and return the new head
     def reverseList(head):
@@ -645,11 +645,8 @@ def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
                 tail.next = nextStart
                 return newHead
 
-        # item after last item in this group is start of next group
+        # remove pointer from last item in this group to start of next group, but save the node
         nextStart = current.next
-
-        # remove pointer from last item in this group to start of next group
-        next = current.next
         current.next = None
 
         # reverse the current group
@@ -663,7 +660,7 @@ def reverseKGroup(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
 
         # set the tail to the start (now end) of this group and move to the next node (start of next group)
         tail = currentStart
-        current = next
+        current = nextStart
         
     return newHead
 
