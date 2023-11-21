bf3fcbbe76b5050ce6d50e37a112fe662e2bf007
Marcus Del Vecchio <marcusdel2112@rogers.com>
2023-11-21 14:26:03 -0500

    complete Leetcode Integer to Roman Medium

--- hashmaps.py
+++ hashmaps.py
@@ -45,3 +45,73 @@ def romanToInt(self, s: str) -> int:
                 i += 1
 
         return res
+
+# Leetcode Integer to Roman Medium
+# https://leetcode.com/problems/integer-to-roman/submissions/
+# Given an integer convert it to a roman numeral.
+# took 19 mins
+# but there is a way simpler and hash map way to do it see https://leetcode.com/problems/integer-to-roman/solutions/6274/simple-solution/
+def intToRoman(self, num: int) -> str:
+        res = []
+
+        # divide by 1000s to get number of Ms
+        M = floor(num/1000)
+        num -= M*1000
+        res.extend(["M"]*M)
+
+        CM = floor(num/900)
+        num -= CM*900
+        res.extend(["CM"]*CM)
+        
+
+        # divide by 500s to get number of Ds
+        D = floor(num/500)
+        num -= D*500
+        res.extend(["D"]*D)
+
+        CD = floor(num/400)
+        num -= CD*400
+        res.extend(["CD"]*CD)
+
+        # divide by 100s to get number of Cs
+        C = floor(num/100)
+        num -= C*100
+        res.extend(["C"]*C)
+
+        XC = floor(num/90)
+        num -= XC*90
+        res.extend(["XC"]*XC)
+
+        # divide by 50 to get number of Ls
+        L = floor(num/50)
+        num -= L*50
+        res.extend(["L"]*L)
+
+        XL = floor(num/40)
+        num -= XL*40
+        res.extend(["XL"]*XL)
+
+        # divide by 10 to get number of Xs
+        X = floor(num/10)
+        num -= X*10
+        res.extend(["X"]*X)
+
+        IX = floor(num/9)
+        num -= IX*9
+        res.extend(["IX"]*IX)
+
+        # divide by 5 to get number of Vs
+        V = floor(num/5)
+        num -= V*5
+        res.extend(["V"]*V)
+
+        IV = floor(num/4)
+        num -= IV*4
+        res.extend(["IV"]*IV)
+
+        # divide by 1 to get number of Is
+        I = floor(num/1)
+        num -= I*1
+        res.extend(["I"]*I)
+    
+        return ''.join(res)
