2d20d2dd1235bab40c4895cd06844131736c0c8c
Marcus Del Vecchio <marcusdel2112@rogers.com>
2023-11-27 16:54:11 -0500

    complete tree symmetry problem

--- trees.py
+++ trees.py
@@ -15,3 +15,52 @@ def maxDepth(self, root: Optional[TreeNode]) -> int:
     left_ans = maxDepth(self, root.left)
     right_ans = maxDepth(self, root.right)
     return max(left_ans, right_ans) + 1
+
+# Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+# https://leetcode.com/explore/learn/card/data-structure-tree/17/solve-problems-recursively/536/
+# ex:   Input: root = [1,2,2,3,4,4,3]
+#       Output: true
+def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+    if not root:
+        return True
+    
+    # func to determine if items are same forward and back
+    def isLevelSymmetric(level_vals):
+        if len(level_vals) == 1: return True
+        
+        first, second = 0, len(level) - 1
+        
+        while first < second:
+            if level_vals[second] != level_vals[first]:
+                return False
+            
+            second -= 1
+            first += 1
+        return True
+    
+    # check symmetry per depth level, so using a queue to manage the items in the level
+    level = deque()
+    level.append(root)
+    
+    while len(level):
+        # check this level first before traversing to next level
+        level_vals = []
+        for node in level:
+            level_vals.append(node.val if node else None)
+        print(level_vals)
+        level_ok = isLevelSymmetric(level_vals)
+        
+        # if the current level isn't symmetric stop
+        if not level_ok:
+            return False
+        
+        # continue to the next level
+        next_level = deque()
+        for node in level:
+            if not node: continue
+            next_level.append(node.left)
+            next_level.append(node.right)
+            
+        level = next_level
+    
+    return True
