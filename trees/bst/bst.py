d4085717682c8d2534b8cfa9c7b8f7ef572ea7ef
Marcus Del Vecchio <marcusdel2112@rogers.com>
2023-12-14 16:34:34 -0500

    complete Convert Sorted Array to Binary Search Tree LeetCode Easy

--- trees/bst/bst.py
+++ trees/bst/bst.py
@@ -6,3 +6,15 @@
 # inerstion
 # deletion
 
+# Convert Sorted Array to Binary Search Tree LeetCode Easy
+def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        
+        def build_tree(arr):
+            if not arr: return None
+            root_idx = len(arr)//2
+            left = build_tree(arr[:root_idx])
+            right = build_tree(arr[root_idx+1:])
+            root = TreeNode(arr[root_idx], left, right)
+            return root
+
+        return build_tree(nums)
