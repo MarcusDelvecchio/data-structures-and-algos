6d81d3db7861b4a44c2ad0d5722aa6432663a1e5
Marcus Del Vecchio <marcusdel2112@rogers.com>
2024-01-29 00:15:55 -0500

    complete Divisor Game LeetCode Easy

---
+++ misc.py
@@ -0,0 +1,6 @@
+# Divisor Game LeetCode Easy
+# https://leetcode.com/problems/divisor-game/description/
+# this question is a brain fork until you understand the maths behind it
+# good explanation https://leetcode.com/problems/divisor-game/solutions/274727/python-dp/
+def divisorGame(self, n: int) -> bool:
+    return n % 2 == 0
