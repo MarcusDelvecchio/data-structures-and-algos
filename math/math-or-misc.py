6a4fbb84a9b8b4ca94d964f434657780d7c00bb5
Marcus Del Vecchio <marcusdel2112@rogers.com>
2024-03-15 18:11:14 -0400

    complete Product of Array Except Self LeetCode Medium

--- math/math-or-misc.py
+++ math/math-or-misc.py
@@ -13,3 +13,20 @@ def pivotInteger(self, n: int) -> int:
         right -= curr
         curr += 1
     return -1
+
+# Product of Array Except Self LeetCode Medium
+# https://leetcode.com/problems/product-of-array-except-self/
+# TC: O(n), SC: O(n)
+# took 12 mins
+def productExceptSelf(self, nums: List[int]) -> List[int]:
+    total_prod, zeros = 1, 0
+    without_zero = 0
+    for num in nums:
+        if num == 0:
+            zeros += 1
+            if zeros == 1:
+                without_zero = total_prod
+        else:
+            without_zero*= num
+        total_prod*= num
+    return [total_prod//num if num != 0 else without_zero if zeros == 1 else 0 for num in nums]
