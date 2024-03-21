b6db59644a57c7ad7475212cf9881cab4407b41b
Marcus Del Vecchio <marcusdel2112@rogers.com>
2024-03-21 01:17:09 -0400

    complete Permutation in String LeetCode Medium

--- sliding-window/sliding-window.py
+++ sliding-window/sliding-window.py
@@ -40,6 +40,8 @@ def minWindow(self, s: str, t: str) -> str:
 # Longest Substring Without Repeating Characters LeetCode Medium
 # https://leetcode.com/problems/longest-substring-without-repeating-characters/
 # TC: O(n), SC: O(n)
+# took 8:30
+# sliding window appraoach
 # approach: expand the window while the items inside are unique, then contract the window to allow it to keep expanding forward, keepign track of the maximum width it becomes
 def lengthOfLongestSubstring(self, s: str) -> int:
     maxx = 0
@@ -57,3 +59,38 @@ def lengthOfLongestSubstring(self, s: str) -> int:
         maxx = max(right - left + 1, maxx)
         right += 1
     return maxx
+
+# todo cleaner approach available by doing 'for r in range(len(s))' rather than 'while right < len(s)'
+
+# Permutation in String LeetCode Medium
+# https://leetcode.com/problems/permutation-in-string/description/
+# TC: O(n), SC: O(1)? Becuase we reassign s1 to a dict?
+# took 18 mins
+def checkInclusion(self, s1: str, s2: str) -> bool:
+    target = len(s1)
+    s1 = Counter(s1)
+    left = 0
+    for right in range(len(s2)):
+        # if the right element is in s1, ensure we haven't used too many of said element
+        if s2[right] in s1:
+
+            # move left forward until that element is available again if it isn't
+            while s1[s2[right]] == 0:
+                if s2[left] in s1:
+                    s1[s2[left]] += 1
+                left += 1
+            
+            # check if we have new longest
+            if right - left + 1 == target:
+                return True
+
+            # decrement availability of this char
+            s1[s2[right]] -= 1
+        
+        # if the right element is not in s1 we cannot expand the window any more, so move left and right to the element after the current right
+        else:
+            while left <= right:
+                if s2[left] in s1:
+                    s1[s2[left]] += 1
+                left += 1
+    return False
