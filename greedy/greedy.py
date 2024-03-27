0175d1a2c18e3290e88b13cafd5e959db5040d95
Marcus Del Vecchio <marcusdel2112@rogers.com>
2024-03-27 17:38:19 -0400

    complete Split a String in Balanced Strings LeetCode Easy

--- greedy/greedy.py
+++ greedy/greedy.py
@@ -4,4 +4,14 @@
 def minimumSum(self, num: int) -> int:
     nums = [c for c in str(num)]
     nums.sort()
-    return int(nums[0] + nums[3]) + int(nums[1] + nums[2]) 
+    return int(nums[0] + nums[3]) + int(nums[1] + nums[2])
+
+# Split a String in Balanced Strings LeetCode Easy
+# 
+def balancedStringSplit(self, s: str) -> int:
+    res, count = 0, 0
+    for i in range(len(s)):
+        if s[i] == "L": count -= 1
+        elif s[i] == "R": count += 1
+        if count == 0: res += 1
+    return res
