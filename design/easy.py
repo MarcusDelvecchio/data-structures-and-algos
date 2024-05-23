5a1ff5d3c3e6a24237f6e8a13ec2fcb310da0d43
Marcus Del Vecchio <marcusdel2112@rogers.com>
2024-05-22 22:59:09 -0400

    complete Design Parking System LeetCode Easy

---
+++ design/easy.py
@@ -0,0 +1,20 @@
+# Design Parking System LeetCode Easy
+# https://leetcode.com/problems/design-parking-system/description/
+class ParkingSystem:
+
+    def __init__(self, big: int, medium: int, small: int):
+        self.big = big
+        self.medium = medium
+        self.small = small  
+
+    def addCar(self, carType: int) -> bool:
+        if carType == 1 and self.big != 0:
+            self.big -= 1
+            return True
+        elif carType == 2 and self.medium != 0:
+            self.medium -= 1
+            return True
+        elif carType == 3 and self.small != 0:
+            self.small -= 1
+            return True
+        return False
